12f17e154b75f29071d42097dd17bd5e197d2ac9
Rindra <rindraramiaramanana@gmail.com>
2022-03-05 18:34:32 +0300

    car fajer

--- back/src/DataFixtures/AppFixtures.php
+++ back/src/DataFixtures/AppFixtures.php
@@ -2,15 +2,25 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Cars;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+
+use Faker;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker = (new Faker\Factory())::create();
+        $faker->addProvider(new Faker\Provider\Fakecar($faker));
+
+        for ($i = 0; $i < 20; $i++) {
+            $car = new Cars();
+            $car->setName($faker->vehicle());
+            $car->setBrand($faker->vehicleBrand());
+            $manager->persist($car);
+        }
 
         $manager->flush();
     }
